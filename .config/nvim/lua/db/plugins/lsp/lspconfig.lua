c5cf4c44f3fdb24218d62d66ec0208558c950046
Dibyendu <dibyendu.biswas@hotstar.com>
2025-03-09 00:33:47 +0530

    fixed lsp errors and warnings

--- .config/nvim/lua/db/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/db/plugins/lsp/lspconfig.lua
@@ -53,11 +53,29 @@ return {
 				opts.desc = "Show line diagnostics"
 				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-				opts.desc = "Go to previous diagnostic"
-				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+				-- Jump to next error/warning
+				opts.desc = "Go to next error"
+				keymap.set("n", "]d", function()
+					vim.diagnostic.goto_next({ severity = { min = vim.diagnostic.severity.ERROR } })
+				end, opts)
 
-				opts.desc = "Go to next diagnostic"
-				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+				-- Jump to previous error/warning
+				opts.desc = "Go to previous error"
+				keymap.set("n", "[d", function()
+					vim.diagnostic.goto_prev({ severity = { min = vim.diagnostic.severity.ERROR } })
+				end, opts)
+
+				-- Jump to next hint/suggestion
+				opts.desc = "Go to next hint/suggestion/warning"
+				keymap.set("n", "]w", function()
+					vim.diagnostic.goto_next({ severity = { max = vim.diagnostic.severity.WARN } })
+				end, opts)
+
+				-- Jump to previous hint/suggestion
+				opts.desc = "Go to previous hint/suggestion"
+				keymap.set("n", "[w", function()
+					vim.diagnostic.goto_prev({ severity = { max = vim.diagnostic.severity.WARN } })
+				end, opts)
 
 				opts.desc = "Show documentation for what is under cursor"
 				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
